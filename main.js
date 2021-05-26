59448b9f550ae172be96c8fee24a3e9dc30e0dd8
raman20 <ramandwivedi20@protonmail.com>
2021-05-27 00:26:00 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,171 @@
+"use strict";
+
+// TS intro
+
+exports.__esModule = true;
+var msg = "hello raman";
+console.log(msg);
+
+/* variable types */
+
+// boolean
+var bool = true;
+
+// number
+var num = 22;
+
+// string
+var name = "Raman";
+var sentence = "My name is " + name + "\nI am learning TS";
+console.log(sentence);
+
+// null
+var Null = null;
+
+// undefined
+var undef = undefined;
+
+// any  (no error given as no type restriction)
+var random = '21';
+random = 22;
+random = true;
+random = {};
+random = null;
+random = undefined;
+random = function () {
+    console.log("a function");
+};
+// random.name will give no error.
+// random() will give no error.
+// random.toLowerCase() will give no error.
+///////////////////////////////////////////////
+
+// number[] 
+var arr1 = [1, 2, 3, 4];
+
+// Array<number>
+var arr2 = [5, 6, 7, 8];
+
+// Array<any>
+var arr4 = [1, 'raman', { "age": 21 }, true];
+
+// tuple [string, number]
+var arr3 = ['Raman', 22];
+
+// enum
+var Color;
+(function (Color) {
+    Color[Color["red"] = 22] = "red";
+    Color[Color["green"] = 23] = "green";
+    Color[Color["blue"] = 24] = "blue";
+})(Color || (Color = {}));
+;
+var temp = Color.green;
+
+// unknown (similar to 'any' type but can't access properties, assertion required)
+var var1 = 10;
+// var1.name -> error
+// var1.toLowerCase() -> error
+var1.toLowerCase(); //assertion
+var1.name; //assertion
+
+
+// multitype
+var multitype;
+multitype = 20;
+multitype = true;
+
+// object
+var obj;
+obj = { name: "raman", age: 21 };
+var obj2;
+obj2 = { age: 21, name: "raman" };
+
+
+// functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(5, 10);
+
+
+// function with optional parameter
+function fun(num1, num2) {
+    if (num2) {
+        return num1 + num2;
+    }
+    return num1 + num2;
+}
+
+
+// default parameter 
+function fun1(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2) {
+        return num1 + num2;
+    }
+    return num1 + num2;
+}
+function fullName() {
+    var person = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        person[_i] = arguments[_i];
+    }
+    console.log(person);
+}
+
+// interface
+
+function fullName1(person) {
+    console.log(person);
+}
+var p1;
+p1 = {
+    fn: "raman",
+    ln: "dwivedi"
+};
+fullName1(p1);
+
+
+// classes
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log('Good Morning ' + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(name) {
+        return _super.call(this, name) || this;
+    }
+    Manager.prototype.work = function () {
+        console.log("to manage a team");
+    };
+    return Manager;
+}(Employee));
+var m1;
+m1 = new Manager("raman");
---
+++ main.ts
@@ -0,0 +1,159 @@
+// TS intro
+
+export { };
+let msg = "hello raman";
+console.log(msg);
+
+/* variable types */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 22;
+
+// string
+let name: string = "Raman";
+let sentence: string = `My name is ${name}
+I am learning TS`;
+console.log(sentence);
+
+// null
+let Null: null = null;
+
+// undefined
+let undef: undefined = undefined;
+
+// any  (no error given as no type restriction)
+let random: any = '21';
+random = 22;
+random = true;
+random = {};
+random = null;
+random = undefined;
+
+random = () => {
+    console.log("a function");
+}
+
+// random.name will give no error.
+// random() will give no error.
+// random.toLowerCase() will give no error.
+
+///////////////////////////////////////////////
+
+
+// number[] 
+let arr1: number[] = [1, 2, 3, 4];
+
+// Array<number>
+let arr2: Array<number> = [5, 6, 7, 8]
+
+// Array<any>
+let arr4: Array<any> = [1, 'raman', { "age": 21 }, true];
+
+// tuple [string, number]
+let arr3: [string, number] = ['Raman', 22];
+
+// enum
+enum Color { red = 22, green, blue };
+let temp: Color = Color.green;
+
+// unknown (similar to 'any' type but can't access properties, assertion required)
+let var1: unknown = 10;
+// var1.name -> error
+// var1.toLowerCase() -> error
+(var1 as string).toLowerCase(); //assertion
+(var1 as { name: string }).name; //assertion
+
+// multitype
+let multitype: number | boolean;
+multitype = 20;
+multitype = true;
+
+// object
+
+let obj: Object;
+obj = { name: "raman", age: 21 };
+
+let obj2: { name: string, age: number };
+obj2 = { age: 21, name: "raman" };
+
+// functions
+
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
+
+add(5, 10);
+
+// function with optional parameter
+function fun(num1: number, num2?: number) {
+    if (num2) {
+        return num1 + num2;
+    }
+    return num1 + num2;
+}
+
+// default parameter 
+function fun1(num1: number, num2: number = 10) {
+    if (num2) {
+        return num1 + num2;
+    }
+    return num1 + num2;
+}
+
+function fullName(...person: any[]) {
+    console.log(person);
+}
+
+// interface
+interface person1 {
+    fn: string;
+    ln?: string;
+}
+
+function fullName1(person: person1) {
+    console.log(person);
+}
+
+let p1: person1;
+
+p1 = {
+    fn: "raman",
+    ln: "dwivedi"
+}
+
+fullName1(p1);
+
+// classes
+
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log('Good Morning ' + this.employeeName);
+    }
+}
+
+let emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(name: string) {
+        super(name);
+    }
+
+    work() {
+        console.log("to manage a team");
+    }
+}
+
+let m1: Manager;
+
+m1 = new Manager("raman");
